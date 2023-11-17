aae344fe79727de96539fdd223a74675abbfda5a
samanthashleyrose <samantha.rose327@gmail.com>
2023-11-16 22:45:40 -0500

    Added quotes array to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+// GLOBAL VARIABLES
+
+const quotes = [
+    'Banana',
+    'Meatloaf',
+    'For Funsies',
+    'Banana Meatloaf',
+    'Bueller… Bueller… Bueller…',
+    "That's just how functions work",
+    'We put code in your code so you can code while you code',
+    'DO NOT MIX PROMISES AND CALLBACKS. It is a source of pain',
+]
+
 // Makes image shake when clicked
 function shakeImage() {
     const image = document.getElementById('shakingImage');
@@ -7,3 +20,4 @@ function shakeImage() {
       image.classList.remove('shake');
     }, 700);
   }
+
